23e3afc3d6e1bc0da8c50b0babe1c200c15f7695
jumkey <jumkey@users.noreply.github.com>
2022-02-19 00:15:59 +0800

    remove

--- config/platforms.h
+++ config/platforms.h
@@ -34,11 +34,8 @@ const struct hw_config supported_platforms[] = {
     {
         .name = "DS3622xs+",
         .pci_stubs = {
-                { .type = VPD_MARVELL_88SE1475, .bus = 0x07, .dev = 0x00, .fn = 0x00, .multifunction = false },
                 { .type = VPD_MARVELL_88SE9235, .bus = 0x09, .dev = 0x00, .fn = 0x00, .multifunction = false },
                 { .type = VPD_MARVELL_88SE9235, .bus = 0x0c, .dev = 0x00, .fn = 0x00, .multifunction = false },
-                { .type = VPD_INTEL_I210,       .bus = 0x0b, .dev = 0x00, .fn = 0x00, .multifunction = false },
-                { .type = VPD_INTEL_I210,       .bus = 0x0d, .dev = 0x00, .fn = 0x00, .multifunction = false },
                 { .type = __VPD_TERMINATOR__ }
         },
         .emulate_rtc = false,
