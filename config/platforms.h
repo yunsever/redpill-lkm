169d8fb1b3e005ea991357562d9d373798277609
pocopico <pasafiogos@gmail.com>
2022-06-28 15:13:46 +0300

    Testing DVA1622

--- config/platforms.h
+++ config/platforms.h
@@ -78,6 +78,26 @@ const struct hw_config supported_platforms[] = {
         }
     },
     {
+        .name = "DVA1622",
+        .pci_stubs = {
+                { .type = VPD_MARVELL_88SE9235,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
+                { .type = __VPD_TERMINATOR__ }
+        },
+        .emulate_rtc = true,
+        .swap_serial = false,
+        .reinit_ttyS0 = true,
+        .fix_disk_led_ctrl = true,
+        .has_cpu_temp = true,
+        .hwmon = {
+            .sys_thermal = { HWMON_SYS_TZONE_NULL_ID },
+            .sys_voltage = { HWMON_SYS_VSENS_NULL_ID },
+            .sys_fan_speed_rpm = { HWMON_SYS_FAN_NULL_ID },
+            .hdd_backplane = { HWMON_SYS_HDD_BP_DETECT_ID, HWMON_SYS_HDD_BP_ENABLE_ID },
+            .psu_status = { HWMON_PSU_NULL_ID },
+            .sys_current = { HWMON_SYS_CURR_NULL_ID },
+        }
+    },
+     {
         .name = "DS920+",
         .pci_stubs = {
                 { .type = VPD_MARVELL_88SE9235,    .bus = 0x01, .dev = 0x00, .fn = 0x00, .multifunction = false },
@@ -97,6 +117,7 @@ const struct hw_config supported_platforms[] = {
             .sys_current = { HWMON_SYS_CURR_NULL_ID },
         }
     },
+
     {
         .name = "DS1621+",
         .pci_stubs = {
