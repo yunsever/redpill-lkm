cde72f00c14dc44b48cf93b6e55420d7808f3c01
pocopico <pasafiogos@gmail.com>
2023-01-07 14:22:46 +0200

    Fixing RS4021xsp failed power supply status

--- shim/bios/bios_psu_status_shim.c
+++ shim/bios/bios_psu_status_shim.c
@@ -1,5 +1,6 @@
 /**
- * Overrides HWMONGetPSUStatusByI2C to provide fake psu status for SA6400
+ * Overrides HWMONGetPSUStatusByI2C to provide fake psu status for SA6400 and RS4021xsp
+ * to find the required symbol you can consult the mfgbios output while booting the dev LKM
  */
 #include "bios_psu_status_shim.h"
 #include "../../common.h"
@@ -12,11 +13,19 @@
 
 static const struct hw_config *hw_config = NULL;
 static override_symbol_inst *HWMONGetPSUStatusByI2C_ovs = NULL;
+static override_symbol_inst *RS4021xspI2CGetPowerInfo_ovs = NULL;
 
 static int HWMONGetPSUStatusByI2C_shim(void)
 {
     return 1;
 }
+
+static int RS4021xspI2CGetPowerInfo_shim(void)
+{
+    return 1;
+}
+
+
 
 int register_bios_psu_status_shim(const struct hw_config *hw)
 {
@@ -24,9 +33,14 @@ int register_bios_psu_status_shim(const struct hw_config *hw)
 
     if (unlikely(HWMONGetPSUStatusByI2C_ovs))
         shim_reg_already();
+    if (unlikely(RS4021xspI2CGetPowerInfo_ovs))
+        shim_reg_already();
+
+
 
     hw_config = hw;
     override_symbol_or_exit_int(HWMONGetPSUStatusByI2C_ovs, "HWMONGetPSUStatusByI2C", HWMONGetPSUStatusByI2C_shim);
+    override_symbol_or_exit_int(RS4021xspI2CGetPowerInfo_ovs, "RS4021xspI2CGetPowerInfo_ovs", RS4021xspI2CGetPowerInfo_shim);
 
     shim_reg_ok();
     return 0;
@@ -38,6 +52,8 @@ int unregister_bios_psu_status_shim(void)
 
     if (unlikely(!HWMONGetPSUStatusByI2C_ovs))
         return 0; //this is deliberately a noop
+    if (unlikely(!RS4021xspI2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
 
     int out = restore_symbol(HWMONGetPSUStatusByI2C_ovs);
     if (unlikely(out != 0)) {
@@ -45,6 +61,15 @@ int unregister_bios_psu_status_shim(void)
         return out;
     }
     HWMONGetPSUStatusByI2C_ovs = NULL;
+
+     int outrs4021xsp = restore_symbol(RS4021xspI2CGetPowerInfo_ovs);
+    if (unlikely(outrs4021xsp != 0)) {
+        pr_loc_err("Failed to restore RS4021xspI2CGetPowerInfo - error=%d", out);
+        return out;
+    }
+    HWMONGetPSUStatusByI2C_ovs = NULL;
+    RS4021xspI2CGetPowerInfo_ovs = NULL;
+
 
     shim_ureg_ok();
     return 0;
@@ -55,6 +80,10 @@ int reset_bios_psu_status_shim(void)
     shim_reset_in();
     put_overridden_symbol(HWMONGetPSUStatusByI2C_ovs);
     HWMONGetPSUStatusByI2C_ovs = NULL;
+
+    put_overridden_symbol(RS4021xspI2CGetPowerInfo_ovs);
+    RS4021xspI2CGetPowerInfo_ovs = NULL;
+
 
     shim_reset_ok();
     return 0;
